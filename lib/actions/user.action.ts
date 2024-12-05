035a44e2827719b6dc5554f904ca41b09cee208c
Arvin Nacional <arvin.nacionalzxc@gmail.com>
2024-12-05 10:05:22 +0800

    try disablling camelcase error in webhook

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -3,7 +3,6 @@
 import User from "@/database/user.model";
 import dbConnect from "../mongoose";
 import { CreateUserParams } from "./shared.types";
-import handleError from "../handlers/error";
 
 export async function createUser(userData: CreateUserParams) {
   try {
@@ -12,6 +11,7 @@ export async function createUser(userData: CreateUserParams) {
     const newUser = await User.create(userData);
     return newUser;
   } catch (error) {
-    return handleError(error, "server") as ActionResponse;
+    console.log(error);
+    throw new Error("Error creating user");
   }
 }
