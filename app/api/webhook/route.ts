a18c0b29e4fb3f2b73420a6ccf0cff85cd21bec5
Arvin Nacional <arvin.nacionalzxc@gmail.com>
2024-12-05 10:28:21 +0800

    added webhook signing secret

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -53,9 +53,9 @@ export async function POST(req: Request) {
 
   // Do something with payload
   // For this guide, log payload to console
-  const { id } = evt.data;
+  // const { id } = evt.data;
   const eventType = evt.type;
-  console.log(`Received webhook with ID ${id} and event type of ${eventType}`);
+  // console.log(`Received webhook with ID ${id} and event type of ${eventType}`);
   // console.log("Webhook payload:", body);
 
   if (eventType === "user.created") {
