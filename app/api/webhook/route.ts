00bc3b01b932d6cd82f3f71feb412ef2b83d70bf
Arvin Nacional <arvin.nacionalzxc@gmail.com>
2024-12-05 10:08:52 +0800

    fixed linting errors on webhook

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -64,7 +64,7 @@ export async function POST(req: Request) {
     // Create user in database
     const mongoUser = await createUser({
       clerkId: id,
-      name: `${first_name} ${last_name ? last_name : ""}`,
+      name: `${first_name} ${last_name || ""}`,
       email: email_addresses[0].email_address,
       picture: image_url,
     });
