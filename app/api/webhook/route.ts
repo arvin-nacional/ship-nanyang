82beecefbea6dc6eba7a85c4bad9cc55e306e551
Arvin Nacional <arvin.nacionalzxc@gmail.com>
2024-12-05 11:56:49 +0800

    changed the response of the webhook

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -53,10 +53,10 @@ export async function POST(req: Request) {
 
   // Do something with payload
   // For this guide, log payload to console
-  // const { id } = evt.data;
+  const { id } = evt.data;
   const eventType = evt.type;
-  // console.log(`Received webhook with ID ${id} and event type of ${eventType}`);
-  // console.log("Webhook payload:", body);
+  console.log(`Received webhook with ID ${id} and event type of ${eventType}`);
+  console.log("Webhook payload:", body);
 
   if (eventType === "user.created") {
     const { id, email_addresses, first_name, last_name, image_url } = evt.data;
@@ -72,5 +72,5 @@ export async function POST(req: Request) {
     return NextResponse.json({ success: true, user: mongoUser });
   }
 
-  return NextResponse.json({ success: true });
+  return new Response("Webhook received", { status: 200 });
 }
