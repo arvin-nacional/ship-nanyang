d3588162b6e1572f38daff52afe2077f9b0c2dfb
Arvin Nacional <arvin.nacionalzxc@gmail.com>
2024-12-31 00:17:49 +0800

    fix any type error

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
 import qs from "query-string";
 import { UrlQueryParams } from "@/app/types";
 import { clsx, type ClassValue } from "clsx";
