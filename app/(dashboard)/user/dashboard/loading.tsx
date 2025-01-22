e9ce42e80ee7ad2d3017c63a703e529f3aedf9c8
Arvin Nacional <arvin.nacionalzxc@gmail.com>
2025-01-22 13:10:20 +0800

    modified navbar

--- app/(dashboard)/user/dashboard/loading.tsx
+++ app/(dashboard)/user/dashboard/loading.tsx
@@ -8,7 +8,7 @@ import GridLoader from "react-spinners/GridLoader";
 const loading = () => {
   return (
     <div className="flex w-full max-sm:flex-col">
-      <div className="p-12 w-full overflow-scroll" style={{ height: "90vh" }}>
+      <div className="p-12 w-full overflow-scroll h-[90vh] max-sm:h-full">
         <p className="h2-semibold text-primary-500 mb-5">All Carts</p>
         <div className="mb-6 flex justify-between gap-5 max-sm:flex-col sm:items-center">
           <LocalSearchbar
--- components/shared/navbar/Navbar.tsx
+++ components/shared/navbar/Navbar.tsx
@@ -87,9 +87,14 @@ const Navbar = () => {
                 Track Your Package
               </Button>
             </Link>
-            <Link href="/signup">
+            <Link href="/signup" className="max-sm:hidden">
               <Button className="rounded-3xl bg-slate-50 px-10 text-primary-500">
                 Register Now
+              </Button>
+            </Link>
+            <Link href="/signin" className="sm:hidden">
+              <Button className="rounded-3xl bg-slate-50 px-10 text-primary-500">
+                Sign In
               </Button>
             </Link>
           </SignedOut>
