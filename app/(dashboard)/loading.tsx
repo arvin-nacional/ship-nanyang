adb45f18e01b06ab74eaf9add120b5df0d2ba796
Arvin Nacional <arvin.nacionalzxc@gmail.com>
2025-01-22 12:08:27 +0800

    added loading

--- app/(dashboard)/loading.tsx
+++ app/(dashboard)/loading.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import GridLoader from "react-spinners/GridLoader";
 
 const Loading = () => {
-  return <div>Loading</div>;
+  return (
+    <div className="flex justify-center items-center size-full">
+      <GridLoader color="#3B82F6" size={15} />
+    </div>
+  );
 };
 
 export default Loading;
