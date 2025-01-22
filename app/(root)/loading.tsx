37a4e8f2515cb7bca3a85e74957fe896b0ea9e09
Arvin Nacional <arvin.nacionalzxc@gmail.com>
2025-01-22 18:20:04 +0800

    try another spinner

--- app/(root)/loading.tsx
+++ app/(root)/loading.tsx
@@ -1,36 +1,14 @@
+"use client";
 import Footer from "@/components/Footer";
 import React from "react";
+import GridLoader from "react-spinners/GridLoader";
 
 const loading = () => {
   return (
     <main className="background-light900_dark200 relative">
       <section className="flex min-h-screen flex-1 flex-col overflow-y-auto justify-center items-center w-full mt-20">
-        <div className="text-center text-white">
-          <svg
-            className="w-16 h-16 animate-spin text-primary-500"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 100 100"
-            preserveAspectRatio="xMidYMid"
-          >
-            <circle
-              cx="50"
-              cy="50"
-              fill="none"
-              stroke="currentColor"
-              strokeWidth="8"
-              r="35"
-              strokeDasharray="164.93361431346415 56.97787143782138"
-            >
-              <animateTransform
-                attributeName="transform"
-                type="rotate"
-                repeatCount="indefinite"
-                dur="1s"
-                keyTimes="0;1"
-                values="0 50 50;360 50 50"
-              ></animateTransform>
-            </circle>
-          </svg>
+        <div className="flex justify-center items-center ">
+          <GridLoader color="#BD1826" size={15} />
         </div>
       </section>
       <Footer />
