684d6c916cd8feb35247a467d8bafffc4637f9f2
Arvin Nacional <arvin.nacionalzxc@gmail.com>
2025-01-22 16:40:56 +0800

    try adding loading again

--- app/(dashboard)/loading.tsx
+++ app/(dashboard)/loading.tsx
@@ -2,7 +2,7 @@
 import React from "react";
 import GridLoader from "react-spinners/GridLoader";
 
-const Loading = () => {
+const loading = () => {
   return (
     <div className="flex justify-center items-center w-full h-screen">
       <GridLoader color="#3B82F6" size={15} />
@@ -10,4 +10,4 @@ const Loading = () => {
   );
 };
 
-export default Loading;
+export default loading;
