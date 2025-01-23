6f0294411fa5fd1b735aa8a597ebfc37656817dd
Arvin Nacional <arvin.nacionalzxc@gmail.com>
2025-01-23 14:33:18 +0800

    fixed paddings

--- components/ShipmentProcess.tsx
+++ components/ShipmentProcess.tsx
@@ -4,7 +4,7 @@ import { process } from "@/constants";
 
 const ShipmentProcess = () => {
   return (
-    <section className="flex items-center justify-center bg-gray-100 px-16 py-20 max-md:px-5 max-sm:py-5 sm:px-14">
+    <section className="flex items-center justify-center bg-gray-100 px-16 py-20 max-md:px-5 max-sm:py-5 sm:px-14 ">
       <div className="grid w-[1200px] max-w-full  gap-10 px-2 pb-6 align-top max-md:mt-10 ">
         <p className="h1-bold ">Our Shipment Process</p>
         <p className="paragraph-regular text-dark-400">
--- components/Solutions.tsx
+++ components/Solutions.tsx
@@ -7,7 +7,7 @@ const Solutions = () => {
   return (
     <>
       <section
-        className="flex items-center justify-center bg-gray-100 px-16 pt-32 pb-20 max-md:px-5 max-sm:py-16 sm:px-14"
+        className="flex items-center justify-center bg-gray-100 px-16 pt-32 max-sm:pt-0 pb-20 max-sm:pb-0 max-md:px-5 max-sm:py-16 sm:px-14"
         id="solutions"
       >
         <div className=" flex w-[1200px] max-w-full flex-row gap-10 px-2 pb-6 align-top max-md:mt-10 max-sm:flex-col-reverse max-lg:flex-col">
