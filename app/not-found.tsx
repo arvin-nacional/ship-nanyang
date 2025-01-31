efee0aacf47557199c898e512bbc282e2297f6f2
Arvin Nacional <arvin.nacionalzxc@gmail.com>
2025-01-31 13:25:23 +0800

    fixed flex-col

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -6,15 +6,15 @@ import React from "react";
 
 const NotFound = () => {
   return (
-    <div className="w-full h-screen flex items-center justify-center">
-      <p className="h2-bold text-primary-500 mb-5">Page Not Found</p>
+    <div className="w-full h-screen flex items-center justify-center flex-col">
+      <p className="h2-bold text-primary-500 mb-12">Page Not Found</p>
 
-      <p className="paragraph-regular text-dark-500">
+      <p className="paragraph-regular text-dark-500 mb-12">
         We could not find the page you were looking for.
       </p>
       <Link href="/user/dashboard">
         <Button className="bg-primary-500 text-light-800">
-          Go back to Dashboard
+          Go back to Home
         </Button>
       </Link>
     </div>
