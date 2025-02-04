883c66797f051251a1e2786c0730d63ada44479a
Arvin Nacional <arvin.nacionalzxc@gmail.com>
2025-02-04 23:38:20 +0800

    added preload='auto'

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,58 +1,50 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { Button } from "./ui/button";
 import Link from "next/link";
-import GridLoader from "react-spinners/GridLoader";
+// import Image from "next/image";
 
 const Hero = () => {
-  const [isVideoLoaded, setIsVideoLoaded] = useState(false);
-
-  useEffect(() => {
-    const videoElement = document.getElementById(
-      "hero-video"
-    ) as HTMLVideoElement;
-    if (videoElement) {
-      videoElement.onloadeddata = () => {
-        setIsVideoLoaded(true);
-      };
-    }
-  }, []);
-
   return (
     <section className="relative flex h-screen w-full items-center justify-center px-16 py-10 pb-14 max-md:px-10 max-md:py-20">
-      {!isVideoLoaded && (
-        <div className="absolute inset-0 flex items-center justify-center bg-gray-200">
-          <div className="flex justify-center items-center ">
-            <GridLoader color="#BD1826" size={15} />
-          </div>
-        </div>
-      )}
+      {/* <Image
+        src="/assets/images/hero-bg-new3.png"
+        alt="Hero Background"
+        layout="fill"
+        objectFit="cover"
+        quality={100}
+        className="z-0"
+      /> */}
+
       <video
-        id="hero-video"
         autoPlay
         loop
         muted
         playsInline
-        className={`absolute inset-0 w-full h-full object-cover ${isVideoLoaded ? "block" : "hidden"}`}
+        preload="auto"
+        className="absolute inset-0 w-full h-full object-cover"
       >
         <source src="/assets/videos/delivery3.mp4" type="video/mp4" />
         Your browser does not support the video tag.
       </video>
-      {isVideoLoaded && (
-        <div className="relative z-10 w-[1200px] max-w-full justify-between py-20 pb-6 max-md:mt-1 max-sm:py-2">
-          <h1 className="h1-semihero mb-10 text-light-900 drop-shadow-lg">
-            Effortless Shipping <br /> from China to Your Doorstep
-          </h1>
-          <h2 className="h3-semibold w-1/2 text-light-900 max-sm:w-full">
-            Order with ease and let us handle the journey. Fast, reliable, and
-            secure logistics tailored to bring your products home.
-          </h2>
-          <div className="mt-10 flex gap-2">
-            <Link href="https://m.me/sdexpressinternational">
-              <Button className="rounded-3xl">Contact Us</Button>
-            </Link>
-          </div>
+
+      <div className="relative z-10 w-[1200px] max-w-full justify-between py-20 pb-6 max-md:mt-1 max-sm:py-2">
+        <h1 className="h1-semihero mb-10 text-light-900 drop-shadow-lg max-sm:h2-title">
+          Empowering Local Businesses <br /> with Reliable Shipping Solutions{" "}
+          <br />
+          for Seamless Operations.
+        </h1>
+        <h2 className="h3-semibold w-1/2 text-light-900 max-sm:w-full drop-shadow-2xl">
+          Order with ease and let us handle the journey. Fast, reliable, and
+          secure logistics tailored to bring your products home.
+        </h2>
+        <div className="mt-10 flex gap-2">
+          <Link href="/signin">
+            <Button className="rounded-3xl bg-light-800 text-primary-500 px-10">
+              Start Now
+            </Button>
+          </Link>
         </div>
-      )}
+      </div>
     </section>
   );
 };
