0c9bcfbe69f555281ac625d74e56028ab92ae356
Arvin Nacional <arvin.nacionalzxc@gmail.com>
2025-02-04 22:23:30 +0800

    removed unused vars

--- components/forms/Order.tsx
+++ components/forms/Order.tsx
@@ -146,29 +146,6 @@ const Order = ({
     });
   }
 
-  const frameworks = [
-    {
-      value: "next.js",
-      label: "Next.js",
-    },
-    {
-      value: "sveltekit",
-      label: "SvelteKit",
-    },
-    {
-      value: "nuxt.js",
-      label: "Nuxt.js",
-    },
-    {
-      value: "remix",
-      label: "Remix",
-    },
-    {
-      value: "astro",
-      label: "Astro",
-    },
-  ];
-
   return (
     <Form {...form}>
       <form
