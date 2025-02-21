f5a3a52c834f6f7081df9afbb40a8b41681347dd
Arvin Nacional <arvin.nacionalzxc@gmail.com>
2025-02-21 14:03:03 +0800

    changed employee description capitalization

--- components/Employees.tsx
+++ components/Employees.tsx
@@ -4,14 +4,14 @@ const Employees = () => {
   const testimonials = [
     {
       quote:
-        "Daniel ensures smooth coordination between sales and shipping operations. They oversee order processing, manage logistics strategies, and guarantee timely deliveries, all while optimizing costs and ensuring customer satisfaction. With a focus on efficiency and reliability, He helps drive our commitment to seamless shipping solutions.",
+        "Daniel ensures smooth coordination between sales and shipping operations. They oversee order processing, manage logistics strategies, and guarantee timely deliveries, all while optimizing costs and ensuring customer satisfaction. With a focus on efficiency and reliability, he helps drive our commitment to seamless shipping solutions.",
       name: "Daniel Loke",
       designation: "Sales Logistic Manager",
       src: "/assets/images/daniel-3.png",
     },
     {
       quote:
-        "Arvin leads the development and maintenance of our technology systems, ensuring everything runs smoothly and securely. They oversee infrastructure, manage system upgrades, and implement innovative solutions to support business growth. With a focus on efficiency and innovation, He helps keep our operations connected and future-ready.",
+        "Arvin leads the development and maintenance of our technology systems, ensuring everything runs smoothly and securely. They oversee infrastructure, manage system upgrades, and implement innovative solutions to support business growth. With a focus on efficiency and innovation, he helps keep our operations connected and future-ready.",
       name: "Arvin Nacional",
       designation: "IT Manager",
       src: "/assets/images/arvin-2.png",
